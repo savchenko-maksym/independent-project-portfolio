acb224e9df64c46e579ddfd7c419dda03d43d48d
savchenko-maksym <maxim.savchenko91@gmail.com>
2025-03-20 18:56:26 +0200

    add fotter

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -2,3 +2,51 @@
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
+import axios from 'axios';
+
+const axios2 = axios.create({
+  baseURL: 'https://portfolio-js.b.goit.study/api',
+});
+
+const refs = {
+  reviewsListElem: document.querySelector('.reviews-list-js'),
+};
+
+async function getAllReviews() {
+  try {
+    const res = await axios2.get('/reviews');
+    return res.data;
+    // return axios2.get('/reviews').then(res => res.data);
+  } catch (error) {
+    console.error('error geting reviews', error);
+    iziToast.error({
+      title: 'Error',
+      message: 'Not found',
+      position: 'topRight',
+    });
+    return null;
+  }
+}
+
+getAllReviews().then(data => {
+  if (!data || data.length === 0) {
+    refs.reviewsListElem.innerHTML = '<p class="error-message">Not found</p>';
+    return;
+  }
+
+  const markup = reviewsTemplate(data);
+  refs.reviewsListElem.innerHTML = markup;
+});
+
+function reviewTemplate(feedback) {
+  const { author, avatar_url, review } = feedback;
+  return `<li class="reviews-list-item">
+        <img class="reviews-list-img" src="${avatar_url}" alt="photo" />
+        <h3 class="reviews-list-subtitle">${author}</h3>
+        <p class="reviews-list-text">${review}</p>
+      </li>`;
+}
+
+function reviewsTemplate(arr) {
+  return arr.map(reviewTemplate).join('');
+}
--- src/js/work-together.js
+++ src/js/work-together.js
@@ -1,4 +1,52 @@
-// Описаний у документації
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+import axios from 'axios';
+
+const refs = {
+  formElem: document.querySelector('.work-togethe-form'),
+};
+
+refs.formElem.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const form = event.target;
+  const formData = new FormData(form);
+  const email = formData.get('email');
+  const comment = formData.get('comment');
+
+  const requestBody = { email, comment };
+
+  try {
+    const response = await axios.post(
+      'https://portfolio-js.b.goit.study/api/requests',
+      requestBody
+    );
+    if (response.status === 201) {
+      const modal = basicLightbox.create(`
+                <div class="modal">
+          <button class="modal-close"><svg class="menu-close-icon" width="34" height="34">
+      <use href="/img/icons.svg#icon-close"></use>
+    </svg></button>
+          <h2 class="modal-title">Thank you for your interest in cooperation!</h2>
+          <p class="modal-text">The manager will contact you shortly to discuss further details and opportunities for cooperation. Please stay in touch.</p>
+        </div>
+        `);
+
+      modal.show();
+      document
+        .querySelector('.modal-close')
+        .addEventListener('click', () => modal.close());
+
+      form.reset();
+    }
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Failed to send request. Please check your input and try again.',
+      position: 'topRight',
+    });
+    console.error('Form submission error:', error);
+  }
+});
